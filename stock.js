1792234b84bcd0be90a2b0d069e2657c418cb7c8
FranciscoBaglietto <fbagliettopiermattei@gmail.com>
2022-06-29 18:32:52 -0300

    URL de imagenes

--- stock.js
+++ stock.js
@@ -4,21 +4,21 @@ const habitaciones = [
         habitacion: "Simple",  //objeto1
         precio: 100,
         disponibilidad: 4,
-        img: `/imagenes/habitacionSimple.jpg`
+        img: `./imagenes/habitacionSimple.jpg`
     },
     {
         id: 2,
         habitacion: "Doble", //objeto2
         precio: 200,
         disponibilidad: 2,
-        img: `/imagenes/habitacionDoble.jpg`
+        img: `./imagenes/habitacionDoble.jpg`
     },
     {
         id: 3,
         habitacion: "Triple",    //objeto3
         precio: 300,
         disponibilidad: 1,
-        img: `/imagenes/habitacionTriple.jpg`
+        img: `./imagenes/habitacionTriple.jpg`
     }
 ];
 
